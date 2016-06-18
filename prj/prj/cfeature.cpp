99529862af07ddcf0152a872466d5d1b54cf4282
hujingshuang <jingshuang_hu@163.com>
2016-06-18 12:50:51 +0800

    add the mixed feature

--- prj/prj/cfeature.cpp
+++ prj/prj/cfeature.cpp
@@ -1,8 +1,12 @@
 
 #include <iostream>
+#include <vector>
+#include <opencv2/legacy/legacy.hpp>
+
 #include "cfeature.h"
 #include "cextfeature.h"
 #include "cpreprocess.h"
+
 
 CFeature::CFeature(ImgWrap *imgWrapSrc1, ImgWrap *imgWrapSrc2)
 {
@@ -19,8 +23,47 @@ CFeature::CFeature(ImgWrap *imgWrapSrc1, ImgWrap *imgWrapSrc2)
 	//ÌáÈ¡ÌØÕ÷
 	if(extfeat)
 	{
-		//extfeat->doit(imgWrapSrc1, &this->mFeatureImgA);
-		//extfeat->doit(imgWrapSrc2, &this->mFeatureImgB);
-		//this->_mixfeature(&mFeatureImgA, &mFeatureImgB, &mFeatureMode);
+		extfeat->doit(imgWrapSrc1, &this->mFeatureImgA);
+		extfeat->doit(imgWrapSrc2, &this->mFeatureImgB);
+		this->_mixedfeature(&mFeatureImgA, &mFeatureImgB, &mFeatureMode);
 	}
+}
+
+void CFeature::_mixedfeature( const CFeatureImg *featImg1, const CFeatureImg *featImg2, CFeatureModel *featMode )
+{
+	// for lbp feature
+	vector<int> feat1(featImg1->lbpfeat);
+	vector<int> feat2(featImg2->lbpfeat);
+	float lbpDistance = 0;
+	for (unsigned int i = 0; i < feat1.size(); ++i)
+	{
+		lbpDistance += (feat1[i] - feat2[i]) * (feat1[i] - feat2[i]);
+	}
+	lbpDistance = sqrt(lbpDistance) / 57;
+	featMode->mixfeat.push_back(lbpDistance);
+
+	// for sift feature
+	BruteForceMatcher<L2<float> > matcher;					// brute force matcher
+	vector<DMatch> matches1to2;								// result of matches 1-->2
+	vector<DMatch> matches2to1;								// result of matches 2-->1
+
+	matcher.match(featImg1->siftfeat, featImg2->siftfeat, matches1to2);
+	matcher.match(featImg2->siftfeat, featImg1->siftfeat, matches2to1);
+
+	float siftDistance = 0;
+	for (unsigned int i = 0; i < matches1to2.size(); ++i)
+	{
+		siftDistance += matches1to2[i].distance;
+	}
+	for (unsigned int i = 0; i < matches2to1.size(); ++i)
+	{
+		siftDistance += matches2to1[i].distance;
+	}
+	siftDistance = siftDistance / (matches1to2.size() + matches2to1.size());
+	featMode->mixfeat.push_back(siftDistance);
+
+	// for gabor feature
+
+	// ...
+
 }
