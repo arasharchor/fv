24c942b580ad8cbf89791ba40109b40d2ced10b4
hujingshuang <jingshuang_hu@163.com>
2016-06-18 19:23:17 +0800

    delete the using namespace

--- prj/prj/cpreprocess.h
+++ prj/prj/cpreprocess.h
@@ -2,14 +2,13 @@
 #define __CPREPROCESS_H
 #pragma once
 
-#include "cpreprocessInt.h"
-#include <stdio.h>
 #include <iostream>
 #include <vector>
-#include "opencv2/opencv.hpp"
 
-using namespace cv;
-using namespace std;
+#include <opencv2/opencv.hpp>
+
+#include "cpreprocessInt.h"
+
 
 class CPreprocess : public CPreprocessInt
 {
@@ -25,20 +24,20 @@ public:
 private:
 	void _do( ImgWrap *imgWrapSrc);
 
-	void _detectObjectsCustom(Mat &img, CascadeClassifier &cascade, vector<Rect> &objects, int scaledWidth, int flags, Size minFeatureSize, float searchScaleFactor, int minNeighbors);
-	void _detectLargestObject(Mat &img, CascadeClassifier &cascade, Rect &largestObject, int scaledWidth = 320);//寻找一个图像的特征
+	void _detectObjectsCustom(cv::Mat &img, cv::CascadeClassifier &cascade, std::vector<cv::Rect> &objects, int scaledWidth, int flags, cv::Size minFeatureSize, float searchScaleFactor, int minNeighbors);
+	void _detectLargestObject(cv::Mat &img, cv::CascadeClassifier &cascade, cv::Rect &largestObject, int scaledWidth = 320);//寻找一个图像的特征
 	//void _detectManyObjects(const ImgWrap *imgWrapSrc, CascadeClassifier &cascade, vector<Rect> &objects, int scaledWidth = 320);//寻找多个图像的特征
-	void _drawFaceImage(const Mat img, vector<Rect> objects);
-	void _drawFaceImage(const Mat img, Rect largestObject);
+	void _drawFaceImage(const cv::Mat img, std::vector<cv::Rect> objects);
+	void _drawFaceImage(const cv::Mat img, cv::Rect largestObject);
 	//添加函数；
 	/* member var */
 private:
-	CascadeClassifier *classifier;
-	vector<Rect> objects;
-	Rect largestObject;
+	cv::CascadeClassifier *classifier;
+	std::vector<cv::Rect> objects;
+	cv::Rect largestObject;
 	int scaledWidth;
 	int flags;
-	Size minFeatureSize;
+	cv::Size minFeatureSize;
 	float searchScaleFactor;
 	int minNeighbors;
 };
